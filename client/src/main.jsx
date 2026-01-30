185988c7b1f3f3a80f5fab3ac55fa1de5ee461e6
sistempar <sistempar@gmail.com>
2026-01-30 18:06:24 -0300

    chore: trigger redeploy with 18:10 version

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-console.log('CLIENT VERSION: 2026-01-30 T 17:05 (Debug)');
+console.log('CLIENT VERSION: 2026-01-30 T 18:10 (Fixes & DB Sync)');
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>

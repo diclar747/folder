1c7cefbff9ec95de87b02b9f1e58f7a07569c0cd
sistempar <sistempar@gmail.com>
2026-01-30 18:34:59 -0300

    chore: version bump to 18:35

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-console.log('CLIENT VERSION: 2026-01-30 T 18:10 (Fixes & DB Sync)');
+console.log('CLIENT VERSION: 2026-01-30 T 18:35 (Fix: Pause Alarm)');
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>

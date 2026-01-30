4497f3989b65c432eeee4b631c68024f07ff11ad
sistempar <sistempar@gmail.com>
2026-01-30 17:06:04 -0300

    chore: add debug version logs and check file

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 import './index.css'
+import App from './App.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+console.log('CLIENT VERSION: 2026-01-30 T 17:05 (Debug)');
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
     <App />
-  </React.StrictMode>,
+  </StrictMode>,
 )
